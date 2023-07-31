babbef950d9f3d712ca6fb8708bb6b43bf4f4194
Massimiliano Donini <mdonini@rnhb.nl>
2023-07-31 16:45:42 +0200

    Align all the Program.cs

--- samples/AppConfigSample/Program.cs
+++ samples/AppConfigSample/Program.cs
@@ -2,9 +2,6 @@ using Microsoft.Extensions.Hosting;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
-    .ConfigureServices(x =>
-    {
-    })
     .Build();
 
-await host.RunAsync();
+host.Run();
